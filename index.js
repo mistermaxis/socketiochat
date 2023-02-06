1ab371e04efbaac475e4a3eeee547f2121b4dc15
Maximiliano Diaz <maximiliano_diaz@outlook.com.ar>
2023-02-06 17:44:46 -0300

    SocketIO basic setup

--- index.js
+++ index.js
@@ -2,9 +2,23 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server);
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log(`Socket "${socket.id}" has connected`);
+
+  socket.on('disconnect', () => {
+    console.log(`Socket "${socket.id}" has disconnected`);
+  });
+  socket.on('chat message', (msg) => {
+    console.log(`Message from ${socket.id}: "${msg}"`);
+    io.emit('chat message', msg);
+  });
 });
 
 server.listen(4000, () => {
